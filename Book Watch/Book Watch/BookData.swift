16a2192495673afbc798b6a9a2b84f9897571bd5
Davlittl7 <70984454+Davlittl7@users.noreply.github.com>
2023-04-28 13:03:14 -0400

    Add files via upload

---
+++ Book Watch/Book Watch/BookData.swift
@@ -0,0 +1,20 @@
+//
+//  BookData.swift
+//  Book Watch
+//
+//  Created by Davonte Littleton on 4/28/23.
+//
+
+import Foundation
+
+struct BookData: Codable {
+    var author: String
+    var country: String
+    //var imageLink: UIImage
+    var language: String
+    var pages: Int
+    var title: String
+    var year: Int
+    
+    
+}
