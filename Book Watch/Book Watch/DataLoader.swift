9e150f562df3600ae402f4f4a21999a78e6f797a
Davlittl7 <70984454+Davlittl7@users.noreply.github.com>
2023-04-29 23:25:51 -0400

    Update DataLoader.swift

--- Book Watch/Book Watch/DataLoader.swift
+++ Book Watch/Book Watch/DataLoader.swift
@@ -1,1 +1,15 @@
+//
+// DataLoader.swift
+// Book Watch
+//
+// Created by Davonte Littleton on 4/29/23.
 
+import Foundation 
+
+public class DataLoader {
+  @Published var bookData = [BookData]()
+  
+  func load() {
+    if let fileLocation = Bundle.main.url(forResource: , withExtension: )
+  }
+}
