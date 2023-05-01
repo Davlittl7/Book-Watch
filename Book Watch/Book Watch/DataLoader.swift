6a044df0a6015135ad2eb7671bef9f2fecabdf3e
Davlittl7 <70984454+Davlittl7@users.noreply.github.com>
2023-05-01 01:15:26 -0400

    Update DataLoader.swift

--- Book Watch/Book Watch/DataLoader.swift
+++ Book Watch/Book Watch/DataLoader.swift
@@ -9,14 +9,26 @@ import Foundation
 public class DataLoader {
   @Published var bookData = [BookData]()
   
+  init() {
+    load()
+    sort()
+  }
   func load() {
     if let fileLocation = Bundle.main.url(forResource: "books", withExtension: "json") {
       //do catch to check for error
       do {
         let data = try Data(contentsOf: fileLocation)
+        let jsonDecoder = JSONDecoder()
+        let dataFromJson = try jsonDecoder.decode([BookData].self, from: data)
+        
+        self.bookData = dataFromJson
       } catch {
         print(error)
       }
     }
+  }
+  
+  func sort() {
+    self.bookData = self.bookData.sorted(by: { $0.title < $1.title }) 
   }
 }
