0ca734dc081e0453fd49af61fc0ca472457da4b9
Davlittl7 <70984454+Davlittl7@users.noreply.github.com>
2023-05-01 17:39:35 -0400

    Add files via upload

---
+++ Book Watch/Book Watch/BookTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  BookTableViewCell.swift
+//  Book Watch
+//
+//  Created by Guest User on 4/28/23.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var homeBookView: UIView!
+    @IBOutlet weak var homeBookInfoLabel: UILabel!
+    
+    @IBOutlet weak var homeAddBookBtn: UIButton!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBAction func HomeAddBookBtnClicked(_ sender: UIButton) {
+    }
+}
