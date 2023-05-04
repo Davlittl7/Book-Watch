7efdfdddbc2c879c89037409f169158b209e8385
Davlittl7 <70984454+Davlittl7@users.noreply.github.com>
2023-05-04 14:51:09 -0400

    Add files via upload

---
+++ Book Watch/Book Watch/SignUpViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SignUpViewController.swift
+//  Book Watch
+//
+//  Created by Guest User on 4/25/23.
+//
+
+import UIKit
+import Firebase
+
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+
+    @IBAction func signUpClicked(_ sender: UIButton) {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { firebaseResult, error in
+            if error != nil {
+                print("error")
+                return
+            } else {
+                //Go back to home screen
+                self.performSegue(withIdentifier: "goToHomeSignUp", sender: self)
+            }
+        }
+        
+    }
+}
