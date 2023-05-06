334c0d4ab52ca2b5f629597c7949f3997358d849
Davlittl7 <70984454+Davlittl7@users.noreply.github.com>
2023-05-06 10:20:02 -0400

    Add files via upload

---
+++ Book Watch/Book Watch/AccountBookTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  AccountBookTableViewCell.swift
+//  Book Watch
+//
+//  Created by Guest User on 5/3/23.
+//
+
+import UIKit
+
+class AccountBookTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var finishBookBtn: UIButton!
+    @IBOutlet weak var accountBookInfoLabel: UILabel!
+    @IBOutlet weak var accountImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    
+    @IBAction func finishBookBtnClicked(_ sender: UIButton) {
+        
+        if let index = HomeScreenViewController.myBooksVC?.accountBooksArr.firstIndex(of: accountBookInfoLabel.text!) {
+            HomeScreenViewController.myBooksVC?.accountBooksArr.remove(at: index)
+            HomeScreenViewController.myBooksVC?.userDefaults.set(HomeScreenViewController.myBooksVC?.accountBooksArr, forKey: "accountBooksArr")
+            HomeScreenViewController.myBooksVC?.MyBooksTableView.reloadData()
+        }
+    }
+}
